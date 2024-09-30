993e01a6102c6c631b53528c60bc212f1b1ddfab
mpadge <mark.padgham@email.com>
2024-09-30 10:47:50 +0200

    reduce summarise to 1 city only

--- src/components/summarise/summarisePage.tsx
+++ src/components/summarise/summarisePage.tsx
@@ -11,40 +11,15 @@ export default function SummarisePage() {
 
     const [idx, setIdx] = useState(0);
 
-    useEffect(() => {
-        var idxLocal = 0;
-        if (typeof window != "undefined") {
-            const storedIdx = localStorage.getItem('uaCityIdx');
-            if(storedIdx) {
-                idxLocal = parseInt(storedIdx, 10);
-                if (isNaN(idxLocal)) {
-                    idxLocal = 0;
-                }
-            }
-        }
-        setIdx(idxLocal);
-    }, [])
-
     const contentArray = Content();
 
     const contentNames = contentArray.map(element => element.name);
     const contentHtml = contentArray.map(element => element.content);
 
-    const handleIdxChange = (idx: number) => {
-        setIdx(idx);
-        if (typeof window != "undefined") {
-            localStorage.setItem("uaCityIdx", idx.toString());
-        }
-    }
-
     return (
         <>
             <div className={styles.summarise}>
                 {contentHtml[idx]}
-                <Control
-                    idx={idx}
-                    handleIdxChange={handleIdxChange}
-                />
             </div>
         </>
     )
