09fed4e0f3c78614f44b21d19e6a140258642fd2
mpadge <mark.padgham@email.com>
2024-09-30 10:53:44 +0200

    rm unknown & unused control import in summarise page

--- src/components/summarise/summarisePage.tsx
+++ src/components/summarise/summarisePage.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect, useState } from "react";
 
-import Control from '@/components/summarise/control';
 import useWindowSize from '@/components/windowSize';
 import styles from '@/styles/summarise.module.css';
 import Content from '@/components/summarise/citySummaryData';
