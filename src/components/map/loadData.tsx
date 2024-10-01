4f994051b7b660a9e8cff9f07cb970a7ee64357d
mpadge <mark.padgham@email.com>
2024-10-01 10:51:00 +0200

    update loadDataFunction for #1

--- src/components/map/loadData.tsx
+++ src/components/map/loadData.tsx
@@ -1,9 +1,9 @@
-export async function loadDataFunction(type: string, handleDataChange: (data: number) => void) {
+export async function loadDataFunction(type: string, setLoadedData: (data: number) => void) {
     const response = await fetch(`/api/gh?type=${type}`);
     if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
     }
     const data = await response.json();
-    handleDataChange(data);
+    setLoadedData(data);
 }
 
