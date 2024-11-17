b956039f52d27f86e4e0cb5af5e17a043134290e
mpadge <mark.padgham@email.com>
2024-11-17 13:40:24 +0100

    update main page for #7

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,36 +45,23 @@ export default function Home() {
 
                 <div className={styles.centerBig}>
                     <p className="text-center">
-                        Urban Analyst
+                        Urban Analyst at Full Resolution
                     </p>
                 </div>
 
                 <div className={styles.centerNormal}>
                     <p className="text-center">
-                        Urban Analyst is a platform for interactive
-                        visualisation and comparison of cities, including
-                        transport systems, measures of accessibility, and
-                        socio-demographic conditions.  There are four main ways
-                        to examine and compare cities: </p> </div>
+                        A demonstration of full-resolution data underlying
+                        the Urban Analyst platform for interactive
+                        visualisation and comparison of cities.
+                        The demonstration can be seen on the 
+                        <Link href="/map" rel="noopener noreferrer">
+                            Map page.
+                        </Link>
+                    </p>
+                </div>
 
                 <div className={styles.grid}>
-
-                    <div className={styles.card}>
-                        <Link href="/summarise" rel="noopener noreferrer">
-                            <div className={styles.cardContent}>
-                                <h2>
-                                    Summarise <span>&#8628;</span>
-                                </h2>
-                                <p className={styles.cardText}>
-                                    Read a summary of analyses for any chosen city.
-                                    The Summary page also identifies the best target
-                                    city for each city, and summarises the ways by
-                                    which that city could best improve to become
-                                    more like the target city.
-                                </p>
-                            </div>
-                        </Link>
-                    </div>
 
                     <div className={styles.card}>
                         <Link href="/map" rel="noopener noreferrer">
